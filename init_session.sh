61504b851d1c403dea4f17c8cd29c83ba35d9984
Lior Blech <nomadbl@gmail.com>
2021-08-14 23:49:13 +0300

    handle vars correctly in script

--- init_session.sh
+++ init_session.sh
@@ -1,9 +1,9 @@
  #!/bin/bash
 echo "Copy s3 privileges"
 addr=$1
-scp -i ~/.ssh/lior.pem -r ~/.aws ec2-user@{addr}:~/
-scp -i ~/.ssh/lior.pem session.zip ec2-user@{addr}:~/session.zip
-ssh -L localhost:8888:localhost:8888 -i ~/.ssh/lior.pem ec2-user@{addr}
+scp -i ~/.ssh/lior.pem -r ~/.aws ec2-user@$(addr):~/
+scp -i ~/.ssh/lior.pem session.zip ec2-user@$(addr):~/session.zip
+ssh -L localhost:8888:localhost:8888 -i ~/.ssh/lior.pem ec2-user@$(addr)
 echo "Copy data from s3..."
 aws s3 cp s3://nomadblfaces/data data.zip
 echo "Copied data from s3"
