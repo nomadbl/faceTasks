e675a91f6b5ee90b02fad59f06ef1804b510afc9
Lior Blech <nomadbl@gmail.com>
2021-08-15 12:24:10 +0300

    working init script

--- init_session.sh
+++ init_session.sh
@@ -1,16 +1,30 @@
  #!/bin/bash
 echo "Copy s3 privileges"
 addr=$1
-scp -i ~/.ssh/lior.pem -r ~/.aws ec2-user@$(addr):~/
-scp -i ~/.ssh/lior.pem session.zip ec2-user@$(addr):~/session.zip
-ssh -L localhost:8888:localhost:8888 -i ~/.ssh/lior.pem ec2-user@$(addr)
+cmd="scp -i ~/.ssh/lior.pem -r ~/.aws ec2-user@${addr}:~/.aws"
+# echo $cmd
+eval $cmd
+cmd="scp -i ~/.ssh/lior.pem session.zip ec2-user@${addr}:~/session.zip"
+# echo $cmd
+eval $cmd
+sshcmd="ssh -i ~/.ssh/lior.pem ec2-user@${addr}"
 echo "Copy data from s3..."
-aws s3 cp s3://nomadblfaces/data data.zip
+cmd="${sshcmd} '"'aws s3 cp s3://nomadblfaces/data data.zip; unzip data.zip'"'"
+# echo $cmd
+eval $cmd
 echo "Copied data from s3"
-echo "unpacking data"
-unzip data.zip
 echo "unpacking session"
-unzip session.zip
+cmd="${sshcmd} '"'unzip session.zip'"'"
+# echo $cmd
+eval $cmd
 echo "starting jupyter"
-jupyter notebook &
-disown -h %1
+cmd="${sshcmd} '"'jupyter notebook &'"'"
+# echo $cmd
+eval $cmd
+cmd="${sshcmd} '"'disown -h %1'"'"
+# echo $cmd
+eval $cmd
+echo "open tunnel"
+cmd="ssh -L localhost:8888:localhost:8888 -i ~/.ssh/lior.pem ec2-user@${addr}"
+# echo "$cmd"
+eval $cmd
