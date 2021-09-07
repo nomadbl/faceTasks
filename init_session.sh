cea6f61edb5d0d87adaa063720b12587434cc0ec
Lior Blech <nomadbl@gmail.com>
2021-09-08 02:21:00 +0300

    pytorch aws init

--- init_session.sh
+++ init_session.sh
@@ -18,15 +18,21 @@ echo "unpacking session"
 cmd="${sshcmd} '"'unzip session.zip'"'"
 # echo $cmd
 eval $cmd
-cmd1="${sshcmd} '"'source activate tensorflow2_latest_p37'"'"
-cmd2="${sshcmd} '"'/home/ec2-user/anaconda3/envs/tensorflow2_latest_p37/bin/python -m pip install --upgrade pip'"'"
-cmd3="${sshcmd} '"'pip install --upgrade tensorflow --pre'"'"
-echo "upgrading tensorflow"
-eval $cmd1
-eval $cmd2
 
-echo "run: jupyter notebook & -> disown -h %1"
+#cmd1="${sshcmd} '"'source activate tensorflow2_latest_p37'"'"
+#cmd2="${sshcmd} '"'/home/ec2-user/anaconda3/envs/tensorflow2_latest_p37/bin/python -m pip install --upgrade pip'"'"
+#cmd3="${sshcmd} '"'pip install --upgrade tensorflow --pre'"'"
+#echo "upgrading tensorflow"
+#eval $cmd1
+#eval $cmd2
+
+cmd1="${sshcmd} '"'source activate pytorch_latest_p37 && yes | pip uninstall torch && yes | pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html && pip3 install tensorboard'"'"
+echo "upgrade pytroch"
+eval $cmd1
+
+#echo "run: jupyter notebook & -> disown -h %1"
 echo "open tunnel"
-echo "$cmd"
-cmd="ssh -L localhost:8888:localhost:8888 -i ~/.ssh/lior.pem ec2-user@${addr}"
+#echo "$cmd"
+cmd="ssh -L localhost:6006:localhost:6006 -i ~/.ssh/lior.pem ec2-user@${addr}"
+echo $cmd
 eval $cmd
